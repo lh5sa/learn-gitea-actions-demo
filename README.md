33c8ce789e66cddc288b236ac0c06344ec722855
liaohui5 <liaohui5@foxmail.com>
2023-12-17 10:45:39 +0800

    add gitea actions config file

---
+++ README.md
@@ -0,0 +1,7 @@
+## 介绍
+
+学习 gitea actions 的示例demo
+
+## 代码
+
+![https://github.com/lh5sa/learn-gitea-actions-demo](https://github.com/lh5sa/learn-gitea-actions-demo)
---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+docker_container_name="learn-gitea-actions-demo"
+docker_image_name="nginx:stable"
+
+# 先停止原来的
+docker stop $docker_container_name
+docker rm $docker_container_name
+
+# 启动容器
+docker run -d \
+	-p 80:80 \
+	-v $(pwd)/docs/.vitepress/dist:/usr/share/nginx/html \
+	--name $docker_container_name $docker_image_name
+
